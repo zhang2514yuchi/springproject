c8676582f284b001ffebd1704ffff53cf59dcf43
zhang2514yuchi <13633458845@163.com>
2022-06-05 21:14:40 +0800

    init

---
+++ springboot/src/main/java/com/example/demo/utils/CodeGenerator.java
@@ -0,0 +1,40 @@
+package com.example.demo.utils;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+
+import java.util.Collections;
+
+
+public class CodeGenerator {
+
+    public static void main(String[] args) {
+        generate();
+    }
+
+    private static void generate() {
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/test?serverTimezone=GMT%2b8", "root", "China0351zyc")
+                .globalConfig(builder -> {
+                    builder.author("张宇驰") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir("D:\\codes\\springproject\\springboot\\src\\main\\java\\"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.example.demo") // 设置父包名
+                            .moduleName(null) // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D:\\codes\\springproject\\springboot\\src\\main\\resources\\mapper\\")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.entityBuilder().enableLombok();
+//                    builder.mapperBuilder().enableMapperAnnotation().build();
+                    builder.controllerBuilder().enableHyphenStyle()  // 开启驼峰转连字符
+                            .enableRestStyle();  // 开启生成@RestController 控制器
+                    builder.addInclude("sys_menu") // 设置需要生成的表名
+                            .addTablePrefix("t_", "sys_"); // 设置过滤表前缀
+                })
+//                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+
+    }
+}
